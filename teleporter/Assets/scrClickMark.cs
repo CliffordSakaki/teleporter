0923ccda824396cd6e18ab5b41e3019cb70c1297
CliffordSakaki <cliffordsakaki@hotmail.com>
2017-11-06 17:44:22 +0900

    project commit

---
+++ teleporter/Assets/scrClickMark.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrClickMark : MonoBehaviour
+{
+
+    Vector3 scale;
+
+    // Use this for initialization
+    void Start()
+    {
+        scale = gameObject.transform.localScale;
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        scale.x -= 0.01f;
+        scale.y -= 0.01f;
+        scale.z -= 0.01f;
+        transform.localScale = scale;
+
+        if (scale.x <= 0.0f) {
+            gameObject.transform.SetParent (null);
+            Destroy (gameObject);
+        }
+    }
+}
