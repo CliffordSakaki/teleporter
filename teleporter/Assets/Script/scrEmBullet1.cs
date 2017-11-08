fc49c47895f8157879d0f062781a3f76cf3355c5
cliffordsakaki <cliffordsakaki@gmail.com>
2017-11-08 10:28:31 +0900

    移動処理基本部

--- teleporter/Assets/Script/scrEmBullet1.cs
+++ teleporter/Assets/Script/scrEmBullet1.cs
@@ -9,15 +9,43 @@ namespace prjTeleporter
     {
 
         // Use this for initialization
-        void Start()
+        void Start ()
         {
 		
         }
-	
+
         // Update is called once per frame
-        void Update()
+        void Update ()
         {
 		
         }
+
+
+        public void setHeadToTarget (Vector3 posTarget)
+        {
+            Head = posTarget - transform.position;
+        }
+
+        private Vector3 head;
+
+        public Vector3 Head {
+
+            set {//進行方向を表す単位ベクトル 
+                head = Vector3.Normalize (value);
+            }
+            get{ return head; }
+        }
+
+        private float speed;
+
+        public float Speed {
+            set{ speed = value; }
+            get{ return speed; }
+        }
+
+        public Vector3 Vector {
+            get{ return Head * Speed; }
+        }
+
     }
 }
